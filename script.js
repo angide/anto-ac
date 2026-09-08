b5a26481e3cbe72d9f0164c184ccee33fde3de7a
angide <50024734+angide@users.noreply.github.com>
2026-09-08 10:37:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function sendWhatsApp(){
   const address = document.getElementById("address").value || "-";
   const schedule = document.getElementById("schedule").value || "-";
   const text = `Halo Anto AC, saya ingin konsultasi/service AC.%0A%0AKota: ${encodeURIComponent(city)}%0ALayanan: ${encodeURIComponent(service)}%0AAlamat: ${encodeURIComponent(address)}%0AJadwal: ${encodeURIComponent(schedule)}`;
-  window.open(`https://wa.me/${6281334748004}?text=${text}`, "_blank");
+  window.open(`https://wa.me/6281334748004?text=${text}`, "_blank");
 }
 
 document.querySelector(".menu-toggle").addEventListener("click",()=>{
